9a30274592869ee63f6abe599be5c006101a5399
Otavio Sartorelli de Toledo Piza <otaviostpiza@gmail.com>
2023-10-14 17:50:55 -0400

    DataContext

--- src/DataContext.js
+++ src/DataContext.js
@@ -1,21 +1,28 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
-const DataContext = createContext();
+// Create the context. The context is an object with two properties: data and setData.
+const DataContext = createContext({
+    data: undefined,
+    setData: () => { },
+});
 
-export const useData = () => useContext(DataContext);
-
+// Simple context provider that stores the data in the local storage.
 export const DataProvider = ({ children }) => {
     const [data, setData] = useState(undefined);
 
+    // Load the data from the local storage when the component is mounted. If there is no data
+    // in the local storage, set the data to an empty string.
     useEffect(() => {
         const localData = localStorage.getItem("data");
         setData(localData ? localData : '');
     }, []);
 
+    // Update the local storage when the data changes.
     useEffect(() => {
         if (data !== undefined) localStorage.setItem("data", data);
     }, [data]);
 
+    // Return the context provider.
     return (
         <DataContext.Provider value={{ data, setData }}>
             {children}
